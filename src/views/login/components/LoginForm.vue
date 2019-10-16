fa9c1d0a0ef13dcf2b60c76ebbde5aeea547b615
guanzhiyuan <756027005@qq.com>
2019-10-16 11:40:41 +0800

    login

---
+++ src/views/login/components/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+	<el-form ref="form" :model="user" label-width="80px">
+		<div class="form_line">
+			<span class="icon_span">
+				<i class="my_icon fa-user"></i>
+			</span>
+			<el-input class="login_input" v-model="user.userName" placeholder="请输入内容" type='text'></el-input>
+		</div>
+		<div class="form_line">
+			<span class="icon_span">
+				<i class="my_icon fa-lock"></i>
+			</span>
+			<el-input class="login_input" v-model="user.pwd" placeholder="请输入内容" type='password'></el-input>
+		</div>
+		<div class="mt_25">
+			<el-button type="primary" @click="login">登录</el-button>
+		</div>
+	</el-form>
+</template>
+
+<script>
+	export default {
+		name: 'LoginForm',
+		data() {
+			return {
+				user: {
+					userName: '',
+					pwd: ''
+				}
+			}
+		},
+		methods: {
+			login() {
+				this.$router.push({ //核心语句
+					path: '/home', //跳转的路径
+				})
+			}
+		},
+		mounted() {}
+	}
+</script>
+
+<style>
+	@import url("../../../fonts/font/font.css");
+	@import url("../../../style/loginForm.css");
+</style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,42 +1,138 @@
 <template>
 	<div>
-		<el-form ref="form" :model="user" label-width="80px">
-			<el-form-item label="用户名">
-				<el-input v-model="user.userName" clearable type="text">
-				</el-input>
-			</el-form-item>
-			<el-form-item label="密码">
-				<el-input v-model="user.pwd" clearable type="password">
-				</el-input>
-			</el-form-item>
-			<el-form-item>
-				<el-button type="primary" @click="login">登录</el-button>
-			</el-form-item>
-		</el-form>
+		<div class="page-sign-container">
+			<div class="signin-header">
+				<img src="../../assets/logo.png" class="logo_img"/>
+			</div>
+			<div class="signin-body">
+				<div class="container">
+					<login-form/>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+	import * as Three from 'three'
+	import { LoginForm } from './components'
+	
 	export default {
 		name: 'Login',
 		data() {
 			return {
-				user: {
-					userName: '',
-					pwd: ''
-				}
+				scene: '',
+				camera: '',
+				renderer: '',
+				container: '',
+				aspect: '',
+				fov: '',
+				plane: '',
+				far: '',
+				mouseX: '',
+				mouseY: '',
+				windowHalfX: '',
+				windowHalfY: '',
+				geometry: '',
+				starStuff: '',
+				materialOptions: '',
+				stars: ''
 			}
 		},
+		components: {
+			LoginForm
+		},
 		methods: {
-			login() {
-				this.$router.push({ //核心语句
-					path: '/home', //跳转的路径
-				})
+			init() {
+				this.container = document.createElement(`div`);
+				document.body.appendChild(this.container);
+
+				this.mouseX = 0;
+				this.mouseY = 0;
+
+				this.aspect = window.innerWidth / window.innerHeight;
+				this.fov = 40;
+				this.plane = 1;
+				this.far = 800;
+
+				this.windowHalfX = window.innerWidth / 2;
+				this.windowHalfY = window.innerHeight / 2;
+
+				this.camera = new Three.PerspectiveCamera(
+					this.fov,
+					this.aspect,
+					this.plane,
+					this.far
+				);
+
+				this.camera.position.z = this.far / 2;
+
+				this.scene = new Three.Scene({
+					antialias: true
+				});
+				this.scene.fog = new Three.FogExp2(0x1b1b1b, 0.0001);
+
+				this.starForge();
+
+				this.renderer = new Three.WebGLRenderer({
+					antialias: true,
+					alpha: true
+				});
+				this.renderer.setPixelRatio((window.devicePixelRatio) ? window.devicePixelRatio : 1);
+				this.renderer.setSize(window.innerWidth, window.innerHeight);
+				this.renderer.autoClear = false;
+				this.renderer.setClearColor(0x000000, 0.0);
+				this.container.appendChild(this.renderer.domElement);
+
+				document.addEventListener('mousemove', this.onMouseMove, false);
+			},
+			starForge() {
+				var amount = 45000;
+				this.geometry = new Three.SphereGeometry(1000, 100, 50);
+
+				this.materialOptions = {
+					color: new Three.Color(0xffffff),
+					size: 1.1,
+					transparency: true,
+					opacity: 0.8
+				};
+
+				this.starStuff = new Three.PointsMaterial(this.materialOptions);
+
+				for(var i = 0; i < amount; i++) {
+					var item = new Three.Vector3();
+					item.x = Math.random() * 2000 - 1000;
+					item.y = Math.random() * 2000 - 1000;
+					item.z = Math.random() * 2000 - 1000;
+
+					this.geometry.vertices.push(item);
+				}
+
+				this.stars = new Three.PointCloud(this.geometry, this.starStuff);
+				this.scene.add(this.stars);
+			},
+			animate() {
+				requestAnimationFrame(this.animate);
+				this.render();
+			},
+			render() {
+				this.camera.position.x += (-this.mouseX - this.camera.position.x) * 0.005;
+				this.camera.position.y += (this.mouseY - this.camera.position.y) * 0.005;
+				this.camera.lookAt(this.scene.position);
+				this.renderer.render(this.scene, this.camera);
+			},
+			onMouseMove(e) {
+				this.mouseX = e.clientX - this.windowHalfX;
+				this.mouseY = e.clientY - this.windowHalfY;
 			}
+		},
+		mounted() {
+			this.init();
+			this.animate();
 		}
 	}
 </script>
 
 <style>
-
+	@import url("../../style/login.css");
 </style>
